00c285c6f1241f624db291ce2b129e32d8bae04d
Asmita Kakkar <asmitakakkar11@gmail.com>
2021-02-08 00:23:43 +0530

    Addition

---
+++ java.java
@@ -0,0 +1,11 @@
+class java
+{
+	public static void main(String arg[])
+	{
+		int n1,n2,sum;
+		n1=Integer.parseInt(arg[0]);
+		n2=Integer.parseInt(arg[1]);
+		sum=n1+n2;
+		System.out.println(sum);		
+	}
+}

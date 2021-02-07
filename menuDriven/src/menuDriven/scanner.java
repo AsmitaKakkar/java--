050a9c7096e75b299036d44e7d3a5d44c0649299
Asmita Kakkar <asmitakakkar11@gmail.com>
2021-02-08 00:10:37 +0530

    How to use Scanner Class

---
+++ menuDriven/src/menuDriven/scanner.java
@@ -0,0 +1,17 @@
+package menuDriven;
+
+import java.util.Scanner;
+
+public class scanner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter your name:");
+		System.out.println("Welcome "+sc.nextLine()+" Greetings!!");
+		System.out.println("Enter your sister's name:");
+		System.out.println("Hey!! "+sc.nextLine()+" Tell me How are you ??");
+		
+	}
+
+}

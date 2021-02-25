5e84800d5b1951ba029dcc109c25351f37f8d777
Asmita Kakkar <asmitakakkar11@gmail.com>
2021-02-26 00:04:09 +0530

    --

---
+++ javaLab/src/javaLab/student_report.java
@@ -0,0 +1,14 @@
+package javaLab;
+
+public class student_report {
+
+	public static void main(String args[]) {
+		// TODO Auto-generated method stub
+		System.out.println("Name of student is "+ args[0]);
+		System.out.println("University rollno. "+ args[1]);
+		System.out.println("Course : "+args[2]);
+		System.out.println("Semester: "+args[3]);
+
+	}
+
+}

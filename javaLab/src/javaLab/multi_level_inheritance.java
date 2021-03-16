147b72a446731a5ac0e69576c0f50a9abfab06ab
Asmita Kakkar <asmitakakkar11@gmail.com>
2021-03-16 23:48:25 +0530

    --

--- javaLab/src/javaLab/multi_level_inheritance.java
+++ javaLab/src/javaLab/multi_level_inheritance.java
@@ -22,30 +22,36 @@ class ceo extends persons{
 		System.out.println("Hey!! i am a ceo of this company!!");
 		
 	}
+}
 
 class interns extends ceo{
 	void juniours()
 	{
-		System.out.println("Hey!! we are the interns here!!");
+		System.out.println("Hey!! we are the interns in this company!!");
 	}
 }
 
 
-
+class blablabla extends interns{
+	int num=10;
+	void customers()
+	
+	{
+		
+		System.out.println("Hey!! we came here for shooping your product!!");
+	}
 }
 
-
-public class multi_level_inheritance {
-
+public class multi_level_inheritance{
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		ceo i=new ceo();
+		blablabla obj=new blablabla();
+		obj.head();
+		obj.seniour();
+		obj.employes();
+		obj.juniours();
+		obj.customers();
+		System.out.println("We are in total "+obj.num);
 		
-		
-		i.head();
-		i.seniour();
-		i.employes();
-
 	}
-
+	
 }

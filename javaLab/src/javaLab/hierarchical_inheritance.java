e34ee0da50ba3566f84f00caaff349b1724b9aaa
Asmita Kakkar <asmitakakkar11@gmail.com>
2021-03-16 23:47:33 +0530

    --

--- javaLab/src/javaLab/hierarchical_inheritance.java
+++ javaLab/src/javaLab/hierarchical_inheritance.java
@@ -1,14 +1,24 @@
 package javaLab;
 
+import java.util.Scanner;
+
+
 class calculator{
 	void menu() {
 		System.out.println("1. Addition\n 2. Substraction\n 3. Multiplication\n 4. Division\n");
-	}
+		
 }
+	
+}
+
 
 class Addition extends calculator{
 	void add(int a, int b)
 	{
+		System.out.println("Enter the values of a and b:");
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
 		int result = a+b;
 		System.out.println("Addition of "+a+" and "+b+" is "+result);
 	}
@@ -18,6 +28,10 @@ class Addition extends calculator{
 class Substraction extends calculator{
 	void sub(int a, int b)
 	{
+		System.out.println("Enter the values of a and b:");
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
 		int result=a-b;
 		System.out.println("Substraction of "+a+" and "+b+" is "+result);
 	}
@@ -26,6 +40,10 @@ class Substraction extends calculator{
 class Multiplication extends calculator{
 	void mult(int a, int b)
 	{
+		System.out.println("Enter the values of a and b:");
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
 		int result=a*b;
 		System.out.println("Multiplication of "+a+" and "+b+" is "+result);
 	}
@@ -35,6 +53,10 @@ class Multiplication extends calculator{
 class Division extends calculator{
 	void div(int a,int b)
 	{
+		System.out.println("Enter the values of a and b:");
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
 		int result=a/b;
 		System.out.println("Division of "+a+" and "+b+" is "+result);
 	}
@@ -44,8 +66,35 @@ public class hierarchical_inheritance {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Addition obj1=new Addition();
+		Substraction obj2=new Substraction();
+		Multiplication obj3=new Multiplication();
+		Division obj4=new Division();
+		obj1.menu();
+		obj2.menu();
+		obj3.menu();
 		
-
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter your choice:");
+		int choice=sc.nextInt();
+		if(choice==1)
+		{
+			obj1.add(choice, choice);
+			
+		}
+		else if(choice==2)
+		{
+			obj2.sub(choice, choice);
+		}
+		else if(choice==3)
+		{
+			obj3.mult(choice, choice);
+		}
+		else if(choice==4)
+		{
+			obj4.div(choice, choice);
+		}
+		else 
+			System.out.println("Wrong choice!!");
 	}
-
 }

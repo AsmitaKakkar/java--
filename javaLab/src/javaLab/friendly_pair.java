45c68212f174899f48b5455158c7b36c17b24e37
Asmita Kakkar <asmitakakkar11@gmail.com>
2021-03-20 08:04:44 +0530

    --

---
+++ javaLab/src/javaLab/friendly_pair.java
@@ -0,0 +1,42 @@
+package javaLab;
+import java.util.Scanner;
+
+public class friendly_pair {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1,num2,sum_num1=0,sum_num2 = 0,i;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter first number:");
+		num1=sc.nextInt();
+		System.out.println("Enter second number:");
+		num2=sc.nextInt();
+		
+		for(i=1;i<num1;i++)
+		{
+			if(num1%i==0)
+			{
+				sum_num1+=i;
+			}
+		}
+		for(i=1;i<num2;i++)
+		{
+			if(num2%i==0)
+			{
+				sum_num2+=i;
+			}
+		}
+		
+		if(sum_num1/num1==sum_num2/num2)
+		{
+			System.out.println(num1 +" "+num2 +" are friendly pairs!! ");
+			
+		}
+		else 
+		{
+			System.out.println(num1+" "+num2+" are not friendly pairs!!");
+		}
+
+	}
+
+}

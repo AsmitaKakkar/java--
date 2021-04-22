433a869404c6cdef12a944a1364d00a840833b9a
Asmita Kakkar <asmitakakkar11@gmail.com>
2021-04-23 01:21:36 +0530

    --

--- code_with_harry/src/code_with_harry/cgpa.java
+++ code_with_harry/src/code_with_harry/cgpa.java
@@ -2,16 +2,21 @@ package code_with_harry;
 
 import java.util.Scanner;
 
-public class cgpa {
+public class cgpa{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Enter the marks obtained in physics:");
-		Scanner sc=new Scanner(System.in);
-		
-
-	}
-		
-	}
+public static void main(String[] args)
+{
+	System.out.println("Enter the marks obtained in physics:");
+	Scanner sc =new Scanner (System.in);
+	float phy=sc.nextFloat();
+	System.out.println("Enter the marks obtained in chemistry:");
+	float chim=sc.nextFloat();
+	System.out.println("Enter the marks obtained in maths:");
+	float math=sc.nextFloat();
+	float total=phy+chim+math;
+	float percent=(total/300)*100;
+	float cgpa=(float) (percent/9.5);
+	System.out.println("Your cgpa in cbse board is: "+cgpa);
+}
 
 }

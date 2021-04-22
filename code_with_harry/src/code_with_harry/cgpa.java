dc60fc7c5200a23a6787c617ec2d6b3366da89a9
Asmita Kakkar <asmitakakkar11@gmail.com>
2021-04-23 00:10:36 +0530

    --

--- code_with_harry/src/code_with_harry/cgpa.java
+++ code_with_harry/src/code_with_harry/cgpa.java
@@ -1,9 +1,17 @@
 package code_with_harry;
+
+import java.util.Scanner;
+
 public class cgpa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Enter the marks obtained in physics:");
+		Scanner sc=new Scanner(System.in);
+		
 
+	}
+		
 	}
 
 }

8459c1614e17af4d5d131d4fee74f00c76f52bbf
Asmita Kakkar <asmitakakkar11@gmail.com>
2021-04-22 23:44:03 +0530

    --

---
+++ code_with_harry/src/code_with_harry/greeting.java
@@ -0,0 +1,16 @@
+package code_with_harry;
+
+import java.util.Scanner;
+
+public class greeting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter your name!!");
+		Scanner sc =new Scanner(System.in);
+		String name=sc.nextLine();
+		System.out.println("\"Hello!! "+name +", have a good day\"");
+
+	}
+
+}

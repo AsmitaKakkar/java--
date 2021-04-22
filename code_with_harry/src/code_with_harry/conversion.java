29ab418289cb3046eae5aafe1f3fab3900589adb
Asmita Kakkar <asmitakakkar11@gmail.com>
2021-04-22 23:45:04 +0530

    --

---
+++ code_with_harry/src/code_with_harry/conversion.java
@@ -0,0 +1,19 @@
+package code_with_harry;
+
+import java.util.Scanner;
+
+public class conversion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter the digits you want to convert:");
+		Scanner sc=new Scanner(System.in);
+		int num=sc.nextInt();
+		System.out.println("\nnumber = "+num+" kilometer");
+		System.out.println("\n\n\t\tKilometer to miles conversion!!\n\n");
+		System.out.println("number = "+num*0.62+" miles");
+		
+		
+	}
+
+}

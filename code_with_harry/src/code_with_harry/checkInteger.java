045b77456099a78ce3ed0b8e152b0b92638a9578
Asmita Kakkar <asmitakakkar11@gmail.com>
2021-04-23 01:22:50 +0530

    --

--- code_with_harry/src/code_with_harry/checkInteger.java
+++ code_with_harry/src/code_with_harry/checkInteger.java
@@ -7,11 +7,16 @@ public class checkInteger {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("\t\tChecking Number\n\n");
-		System.out.println("Enetr the number you want to check!!");
+		System.out.println("Enter the number you want to check!!");
 		Scanner sc=new Scanner(System.in);
-		int num=sc.nextInt();
+		double num=sc.nextDouble();
+			if(num%1==0)
+			{
+				System.out.println("yes!! "+num +" is an integer");
+			}
+			else {
+				System.out.println(num+" is not an integer");
+			}
+		}
 		
-
 	}
-
-}

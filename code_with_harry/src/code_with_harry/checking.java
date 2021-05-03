8812a89fb52f4c6a575df647f4b3b14ecabd0a34
Asmita Kakkar <asmitakakkar11@gmail.com>
2021-05-04 00:05:39 +0530

    --

---
+++ code_with_harry/src/code_with_harry/checking.java
@@ -0,0 +1,20 @@
+package code_with_harry;
+
+public class checking {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double result=12+12.22d;
+		System.out.println(result);
+		int y=10;
+		float x=++y*8;
+		System.out.println(x);
+		
+		char a='c';
+		a++;
+		System.out.println(a);
+		
+
+	}
+
+}

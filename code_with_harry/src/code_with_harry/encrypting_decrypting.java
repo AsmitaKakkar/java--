b83d8673cf0beb5c77aff79c10d79bf93329be77
Asmita Kakkar <asmitakakkar11@gmail.com>
2021-05-06 23:46:36 +0530

    --

--- code_with_harry/src/code_with_harry/encrypting_decrypting.java
+++ code_with_harry/src/code_with_harry/encrypting_decrypting.java
@@ -1,9 +1,18 @@
 package code_with_harry;
 
+import java.util.Scanner;
+
 public class encrypting_decrypting {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Enter the grade you obtained:");
+		Scanner sc=new Scanner(System.in);
+		char grade=sc.next().charAt(0);
+		System.out.println("Encrypted Grade :----- "+grade);
+		grade=(char) (grade+8);
+		System.out.println("Decrypted Grade :---- "+grade);
+		
 
 	}
 

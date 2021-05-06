525c2ecbe95a7fc3f6dc42223de928676a5fbe70
Asmita Kakkar <asmitakakkar11@gmail.com>
2021-05-07 00:16:50 +0530

    --

--- code_with_harry/src/code_with_harry/expression.java
+++ code_with_harry/src/code_with_harry/expression.java
@@ -5,7 +5,9 @@ public class expression {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		float a=7/4.0f*9/2f;
-		System.out.println("result = "+a);
+		System.out.println("result1 = "+a);
+		float b=7*49/7.0f+35/7.0f;
+		System.out.println("result2 = "+b);
 		
 
 	}

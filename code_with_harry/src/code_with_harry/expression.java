a4edb36155183d79668585adfae24cba68a8a67f
Asmita Kakkar <asmitakakkar11@gmail.com>
2021-05-06 22:59:25 +0530

    --

---
+++ code_with_harry/src/code_with_harry/expression.java
@@ -0,0 +1,13 @@
+package code_with_harry;
+
+public class expression {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float a=7/4.0f*9/2f;
+		System.out.println("result = "+a);
+		
+
+	}
+
+}

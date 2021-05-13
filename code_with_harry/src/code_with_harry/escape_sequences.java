43596d4749a7172e31d00bdba4d9ca9f1ede92c6
Asmita Kakkar <asmitakakkar11@gmail.com>
2021-05-13 16:16:10 +0530

    --

---
+++ code_with_harry/src/code_with_harry/escape_sequences.java
@@ -0,0 +1,12 @@
+package code_with_harry;
+
+public class escape_sequences {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String letter="\"Dear Harry\", \n\tThis java course is nice!!\n \"Thanks !!\"";
+		System.out.println(letter);
+
+	}
+
+}

fef2362173feaf3505ebb9533ad0d0e32f1d025f
Asmita Kakkar <asmitakakkar11@gmail.com>
2021-05-13 15:27:47 +0530

    --

---
+++ code_with_harry/src/code_with_harry/strings_toLowerCase.java
@@ -0,0 +1,17 @@
+package code_with_harry;
+
+import java.util.Scanner;
+
+public class strings_toLowerCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a string in upper case characters!!");
+		String str = sc.nextLine();
+		String new_str =str.toLowerCase();
+		System.out.println(new_str);
+		
+	}
+
+}

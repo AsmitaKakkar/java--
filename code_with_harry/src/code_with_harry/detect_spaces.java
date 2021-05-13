3a4054a8093e611a369e87c79136b2eecdabc645
Asmita Kakkar <asmitakakkar11@gmail.com>
2021-05-13 16:14:16 +0530

    --

--- code_with_harry/src/code_with_harry/detect_spaces.java
+++ code_with_harry/src/code_with_harry/detect_spaces.java
@@ -1,9 +1,18 @@
 package code_with_harry;
 
+import java.util.Scanner;
+
 public class detect_spaces {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("To detect double and triple sppaces in a string!!");
+		System.out.println("Enter a string!!");
+		Scanner sc =new Scanner (System.in);
+		String str=sc.nextLine();
+		System.out.println(str.lastIndexOf("  "));
+		System.out.println(str.lastIndexOf("   "));
+		
 
 	}
 

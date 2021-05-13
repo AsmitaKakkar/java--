46aa4418f11ab779ce7ccf5fc77cf221a689066e
Asmita Kakkar <asmitakakkar11@gmail.com>
2021-05-13 15:26:50 +0530

    --

---
+++ code_with_harry/src/code_with_harry/replace_name.java
@@ -0,0 +1,13 @@
+package code_with_harry;
+
+public class replace_name {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="Dear <|name|> , Thanks a lot!!";
+		System.out.println(str);
+		System.out.println(str.replace("<|name|>","Asmita"));
+
+	}
+
+}

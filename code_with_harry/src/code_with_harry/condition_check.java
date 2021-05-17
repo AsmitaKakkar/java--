0f64153e81dbd1e804767d8a0db85dc0bfdb4769
Asmita Kakkar <asmitakakkar11@gmail.com>
2021-05-17 21:54:51 +0530

    --

---
+++ code_with_harry/src/code_with_harry/condition_check.java
@@ -0,0 +1,19 @@
+package code_with_harry;
+
+public class condition_check {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		if(a==11) {
+			System.out.println("I am 11!!");
+			
+		}
+		else
+		{
+			System.out.println("I am not 11!");
+		}
+
+	}
+
+}

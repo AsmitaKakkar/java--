e5376807367552459e4189308bf540e537fabaa2
Asmita Kakkar <asmitakakkar11@gmail.com>
2021-05-25 02:39:16 +0530

    --

---
+++ code_with_harry/src/code_with_harry/Reverse_order_table.java
@@ -0,0 +1,16 @@
+package code_with_harry;
+
+import java.util.Scanner;
+
+public class Reverse_order_table {
+	
+	public static void main(String [] args)
+	{
+		System.out.println("***************Table of 10 in reverse order*************");
+		for(int i=10;i>0;i--)
+		{
+			System.out.println("10 * "+i+" = "+10*i);
+		}
+		
+	}
+}

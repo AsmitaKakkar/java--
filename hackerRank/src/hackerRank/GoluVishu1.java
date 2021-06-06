eca8d1061c2ec662cc24b378a5d6567a6278595f
Asmita Kakkar <asmitakakkar11@gmail.com>
2021-06-07 01:05:38 +0530

    --

---
+++ hackerRank/src/hackerRank/GoluVishu1.java
@@ -0,0 +1,22 @@
+package hackerRank;
+
+import java.util.Scanner;
+
+public class GoluVishu1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		        Scanner sc=new Scanner(System.in);
+		        int integer=sc.nextInt();
+		        double doub=sc.nextDouble(); 
+		        sc.nextLine();
+		        String str=sc.nextLine();
+		        System.out.print("String:");
+		        System.out.println(str);
+		        System.out.println("Double: "+doub);
+		        System.out.println("Integer :"+integer);
+		        
+		    }
+
+	}

776501f832880bcbe5fe582d1ab3e97ed8c09104
Asmita Kakkar <asmitakakkar11@gmail.com>
2021-06-07 01:07:02 +0530

    --

--- code_with_harry/src/code_with_harry/pattern.java
+++ code_with_harry/src/code_with_harry/pattern.java
@@ -9,7 +9,7 @@ public class pattern {
 		System.out.println("Enter the limit!!");
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
-		for(int i=1;i<=n;i++)
+		for(int i=n;i>=0;i--)
 		{
 			for(int j=1;j<=i;j++)
 			{

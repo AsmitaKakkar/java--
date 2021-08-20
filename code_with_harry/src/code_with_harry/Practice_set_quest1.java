a75076698b42bc8d9a3c235c0c7701c524bf2d4b
Asmita Kakkar <asmitakakkar11@gmail.com>
2021-08-21 01:44:43 +0530

    --

---
+++ code_with_harry/src/code_with_harry/Practice_set_quest1.java
@@ -0,0 +1,16 @@
+package code_with_harry;
+
+abstract class Pen{
+	
+	abstract void write();
+	abstract void refill();
+}
+
+public class Practice_set_quest1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

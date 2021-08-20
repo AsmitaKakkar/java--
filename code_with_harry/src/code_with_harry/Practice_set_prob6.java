dbb66bcf39c1bb49ba43d02aa12666db6199c183
Asmita Kakkar <asmitakakkar11@gmail.com>
2021-08-21 01:43:54 +0530

    --

---
+++ code_with_harry/src/code_with_harry/Practice_set_prob6.java
@@ -0,0 +1,30 @@
+package code_with_harry;
+
+interface tvRemote
+{
+	void power();
+	
+	void mute();
+	
+	void settings();
+	
+	void volume();
+}
+
+interface smarttvRemote extends tvRemote
+{
+	void ledLights();
+	void voiceSearch();
+	
+}
+
+public class Practice_set_prob6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+	}
+
+}

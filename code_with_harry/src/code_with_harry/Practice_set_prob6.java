5b6a545841cb5d4a03806050c0bacf6889b31f80
Asmita Kakkar <asmitakakkar11@gmail.com>
2021-08-23 03:21:33 +0530

    --

--- code_with_harry/src/code_with_harry/Practice_set_prob6.java
+++ code_with_harry/src/code_with_harry/Practice_set_prob6.java
@@ -18,12 +18,50 @@ interface smarttvRemote extends tvRemote
 	
 }
 
+class bacha implements smarttvRemote
+{
+	public void ledLights()
+	{
+		System.out.println("Turn on the LED lights of the TV cabinet !!");
+	}
+	
+	public void voiceSearch()
+	{
+		System.out.println("Alexa !! listen to me !!");
+	}
+	
+	public void power()
+	{
+		System.out.println("Switch on the tv !!");
+	}
+	
+	public void mute ()
+	{
+		System.out.println("Mute out the volume !!");
+	}
+	
+	public void settings()
+	{
+		System.out.println("Want to change the settings ? \n Click on red button please!!");
+	}
+	
+	public void volume()
+	{
+		System.out.println("Please increase the volume upto some points !!");
+	}
+}
+
 public class Practice_set_prob6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		bacha b= new bacha();
 		
+		b.power();
+		b.mute();
+		b.ledLights();
+	
 
 	}
 

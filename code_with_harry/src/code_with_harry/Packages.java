8fdab84c38c0bcfca9e55a304cc2f401e36bb697
Asmita Kakkar <asmitakakkar11@gmail.com>
2021-08-23 03:20:56 +0530

    --

---
+++ code_with_harry/src/code_with_harry/Packages.java
@@ -0,0 +1,22 @@
+package code_with_harry;
+
+//import java.util.Scanner;    // Imports only the scanner package !!
+
+import java.util.*;   // Imports everything in the package along with the scanner package!!
+
+public class Packages {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc=new Scanner(System.in);
+		
+		//java.util.Scanner sc=new java.util.Scanner(System.in);
+		
+		System.out.println("Enter any number !!");
+		int a=sc.nextInt();
+		System.out.println("this is my scanner taking input as -> "+a);
+
+	}
+
+}

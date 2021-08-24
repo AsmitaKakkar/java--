970818dd97eec5bf1912a023fb2f28b1ec2f760f
Asmita Kakkar <asmitakakkar11@gmail.com>
2021-08-25 03:42:46 +0530

    --

--- code_with_harry/src/code_with_harry/Access_modifiers_in_Java.java
+++ code_with_harry/src/code_with_harry/Access_modifiers_in_Java.java
@@ -7,7 +7,7 @@ class c1{
 	int c=50;
 	private int d=60;
 	
-	void meth1()
+	 void meth1()
 	{
 		System.out.println("a->"+a);
 		System.out.println("b->"+b);
@@ -17,12 +17,28 @@ class c1{
 }
 
 public class Access_modifiers_in_Java {
+	public int a=30;
+	protected int b=40;
+	int c=50;
+	private int d=60;
+	
+	 void meth1()
+		{
+			System.out.println("a->"+a);
+			System.out.println("b->"+b);
+			System.out.println("c->"+c);
+			System.out.println("d->"+d);
+		}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		c1 c=new c1();
-		System.out.println(a);
+		//c.meth1();
+		System.out.println(c.a);
+		System.out.println(c.b);
+		System.out.println(c.c);
+		// System.out.println(c.d);   ->  we cannot access a private modifier outside of its class even not in same package !!
 
 	}
 

f150b8daaa1fd8b09cebe72e5de09ae7145053cd
Asmita Kakkar <asmitakakkar11@gmail.com>
2021-08-25 03:48:03 +0530

    --

---
+++ code_with_harry/src/folder/folderl1/folderl2/problem3.java
@@ -0,0 +1,13 @@
+package folder.folderl1.folderl2;
+
+public class problem3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This problem is user to make one folder inside of another back to back with the help of packages!! ");
+		System.out.println("folder -> folderl1 -> folderl2");
+
+	}
+
+}
